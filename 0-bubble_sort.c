155dec240cace7013c0a594a5092575954ba5ec5
Birtukan Kuma <birtukankuma1113@gmail.com>
2023-11-19 22:42:03 -0800

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,27 +1,25 @@
 #include "sort.h"
-
 /**
- * bubble_sort - a function that sorts an array using bubble sort
- * @array: an array to be sorted
- * @size: size of an array
- * Return: void
+ * bubble_sort - sort array lements from min to max value
+ * @array: array
+ * @size: array size
  */
 void bubble_sort(int *array, size_t size)
 {
-size_t i, tmp = 0, j;
-if (size < 2)
-	return;
-for (i = 0; i < size; i++)
-{
-for (j = 0; j < size; j++)
-{
-if (array[j] > array[j + 1] && array[j + 1])
-{
-tmp = array[j];
-array[j] = array[j + 1];
-array[j + 1] = tmp;
-print_array(array, size);
-}
-}
-}
+
+	size_t i, index, tmp = 0;
+
+	if (size < 2)
+		return;
+	for (i = 0; i < size; i++)
+		for (index = 0; index < size; index++)
+		{
+			if (array[index] > array[index + 1] && array[index + 1])
+			{
+			tmp = array[index];
+			array[index] = array[index + 1];
+			array[index + 1] = tmp;
+			print_array(array, size);
+			}
+		}
 }
